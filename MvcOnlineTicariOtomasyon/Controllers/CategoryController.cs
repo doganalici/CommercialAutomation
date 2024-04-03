2c764593b025afd0329c3244049c59b19c542741
Dogan Alici <doganalici2144@gmail.com>
2024-04-03 05:07:51 +0300

    Update 0.2

--- MvcOnlineTicariOtomasyon/Controllers/CategoryController.cs
+++ MvcOnlineTicariOtomasyon/Controllers/CategoryController.cs
@@ -3,15 +3,44 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MvcOnlineTicariOtomasyon.Models.Classes;
 
 namespace MvcOnlineTicariOtomasyon.Controllers
 {
     public class CategoryController : Controller
     {
         // GET: Category
+        Context c = new Context();
         public ActionResult Index()
         {
+            var degerler=c.Categories.ToList(); ;
+            return View(degerler);
+        }
+        [HttpGet]
+        public ActionResult CategoryAdd()
+        {
             return View();
+        }
+        [HttpPost]
+        public ActionResult CategoryAdd(Category ct)
+        {
+            c.Categories.Add(ct);
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult CategoryDelete(int id)
+        {
+            var ctg = c.Categories.Find(id);
+            c.Categories.Remove(ctg);
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult CategoryGet(int id)
+        {
+            var categori = c.Categories.Find(id);
+            return View("CategoryGet", categori);
         }
     }
 }
